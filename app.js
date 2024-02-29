daa424bfa4404927d652e25208cde2952aace22b
Angelica <mangelicazaragoza@gmail.com>
2024-02-29 11:29:37 +0800

    revert

--- app.js
+++ app.js
@@ -1,36 +1,28 @@
 const $lis = document.querySelectorAll('ul li');
-const $result = document.querySelector('.result');
-const $errorMessage = document.querySelector('.error-message');
 
 $lis.forEach((node) => {
     node.addEventListener('mousedown', function(e) {
         const value = node.innerText.trim();
+        const $result = document.querySelector('.result');
         const resultText = $result.innerText.trim();
 
-        if (resultText === '0' || resultText === 'undefined' || resultText === 'Infinity') {
-            $result.innerText = '';
+        if(resultText == '0' || resultText == 'undefined' || resultText == 'Infinity') {
+            $result.innerText = ''
         }
 
-        if (value === '=') {
-            try {
-                let solution = eval(resultText);
-                if (isNaN(solution) || !isFinite(solution)) {
-                    throw new Error('Invalid expression');
-                }
-                $result.innerText = solution;
-            } catch (err) {
-                $result.innerText = 'Error';
-                $errorMessage.textContent = 'Invalid Expression';
-            }
+        if(value == '=') {
+            let solution = eval(resultText)
+            $result.innerText = solution;
             return true;
         }
 
-        if (value.toLowerCase() === 'c') {
+        if(value.toLowerCase() == "c") {
             $result.innerText = '';
-            $errorMessage.textContent = '';
             return true;
         }
 
         $result.append(value);
-    });
-});
+
+
+    })
+})
